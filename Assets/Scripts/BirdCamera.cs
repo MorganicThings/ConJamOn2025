1b23e981e5a9a2df7a135b813003fc52bc3a7753
MorganicThings <morganic.things@gmail.com>
2025-03-13 03:36:48 +0100

    Prototipo Vuelo

--- Assets/Scripts/BirdCamera.cs
+++ Assets/Scripts/BirdCamera.cs
@@ -7,26 +7,31 @@ namespace Scripts.Airplanes
 {
     public class BirdCamera : MonoBehaviour
     {
+
         [SerializeField] Transform targetTransform;
-        Vector3 offset;
+
+        Vector3 cameraDeltaPos;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cameraDeltaPos = transform.position - targetTransform.position;
+
+            /*if (!Application.isEditor)*/
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+            transform.position = targetTransform.position + cameraDeltaPos.z * targetTransform.forward + cameraDeltaPos.y * targetTransform.up;
+            transform.LookAt(new Vector3(targetTransform.position.x, targetTransform.position.y + cameraDeltaPos.y, targetTransform.position.z));
+        }
 
         private void Awake()
         {
             Assert.IsNotNull(targetTransform, "ERROR olvidaste el target");
-            offset = this.transform.position - targetTransform.position;
-        }
-
-        private void LateUpdate()
-        {
-            // Al profe no le gusta un pelo comparar con null
-            if (targetTransform != null)
-            {
-                // Conservamos la posición x e y de la cámara pero modificamos la z
-                transform.position = new Vector3(
-                    x: transform.position.x,
-                    y: transform.position.y,
-                    z: targetTransform.position.z + offset.z);
-            }
         }
     }
 }
--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -5,47 +6,77 @@ namespace Scripts.Airplanes
 {
     public class BirdController : MonoBehaviour
     {
-        [SerializeField, Range(5f, 20f), Tooltip("Velocidad avance m/s")] float forwardSpeed = 10f;
-        [SerializeField, Range(2f, 10f), Tooltip("Velocidad vertical m/s")] float verticalSpeed = 5f;
-        [SerializeField, Range(2f, 10f), Tooltip("Velocidad horizontal m/s")] float horizontalSpeed = 5f;
-        [SerializeField, Range(30f, 720f), Tooltip("Velocidad rotacion grados/s")] float rollSpeed = 90f;
-        [SerializeField, Min(5f), Tooltip("Mínimo valor x")] float minX = 111.5f;
-        [SerializeField, Min(10f), Tooltip("Máximo valor x")] float maxX = 119.8f;
-        [SerializeField, Min(5f), Tooltip("Mínimo valor y")] float minY = 7.4f;
-        [SerializeField, Min(10f), Tooltip("Máximo valor y")] float maxY = 12.5f;
+        [SerializeField, Range(5f, 1000f), Tooltip("Velocidad avance m/s")] float forwardSpeed = 10f;
+        [SerializeField, Range(30f, 760f), Tooltip("Velocidad rotacion grados/s")] float rollSpeed = 90f;
         [SerializeField, Range(20f, 180f), Tooltip("Máxima rotación")] float maxRoll = 90f;
-        [SerializeField, Range(0.1f, 2f)] float turbulences = 1f;
         [SerializeField, Range(0.5f, 5f), Tooltip("Margen de error en la rotación")] float epsilon = 1f;
         //[SerializeField] ParticleSystem particleSystemPrefab;
+
+        [Range(0, 1000)]
+        public float xSensitivity = 100f;
+
+        [Range(0, 1000)]
+        public float ySensitivity = 100f;
+
+
+        Rigidbody rb;
 
         float verticalInput;
         float horizontalInput;
 
+
+        float mouseX;
+        float mouseY;
+        float xRotation;    
+        float yRotation;
+
         private void Awake()
         {
+            rb = GetComponent<Rigidbody>();
             //Assert.IsNotNull(particleSystemPrefab, "ERROR: olvidaste las partículas");
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
         private void Update()
         {
+            horizontalInput = Input.GetAxisRaw(Constants.HORIZONTAL_AXIS);
+            mouseX = Input.GetAxisRaw("Mouse X") * xSensitivity * Time.deltaTime;
+            mouseY = Input.GetAxisRaw("Mouse Y") * ySensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+            yRotation += mouseX;
+
+            transform.SetPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(xRotation, yRotation, transform.rotation.eulerAngles.z)));
+
             verticalInput = Input.GetAxis(Constants.VERTICAL_AXIS);
             horizontalInput = Input.GetAxis(Constants.HORIZONTAL_AXIS);
 
-            UpdateTranslation();
+            UpdateVelocity();
             UpdateRotation();
+        }
+
+        private void UpdateVelocity() 
+        {
+            rb.velocity = forwardSpeed * transform.forward;
         }
 
         private void UpdateRotation()
         {
-            var rotationInput = -horizontalInput;
+            var rotationInput = Mathf.Clamp(-mouseX - horizontalInput, -1f, 1f);
             var eulerZ = transform.rotation.eulerAngles.z;
             float deltaRotation = rollSpeed * Time.deltaTime * rotationInput;
 
-            // Recuperar rotación si no pulsamos la tecla
-            if (horizontalInput == 0 && !(eulerZ <= epsilon || eulerZ >= 360 - epsilon))
+            //Recuperar rotación si no pulsamos la tecla
+            if (rotationInput == 0 && !(eulerZ <= epsilon || eulerZ >= 360 - epsilon))
             {
                 rotationInput = eulerZ > 0 && eulerZ < maxRoll + epsilon ? -1 : 1;
-                deltaRotation = rollSpeed * Time.deltaTime * rotationInput;
+                deltaRotation = (rollSpeed / 3) * Time.deltaTime * rotationInput;
             }
 
             // // Creamos límites de rotación
@@ -57,44 +88,9 @@ namespace Scripts.Airplanes
             if ((rotationInput > 0 || rotationInput < 0)
                 && (eulerZ + deltaRotation <= maxRoll || eulerZ + deltaRotation >= 360 - maxRoll))
             {
-                transform.Rotate(deltaRotation * Vector3.forward);
+                transform.Rotate(0, 0, deltaRotation);
             }
 
-        }
-
-        private void UpdateTranslation()
-        {
-            float deltaVerticalTranslation = Time.deltaTime * verticalInput * verticalSpeed;
-            Vector3 verticalTranslation = deltaVerticalTranslation * Vector3.up;
-            Vector3 horizontalTranslation = Time.deltaTime * horizontalInput * horizontalSpeed * Vector3.right;
-            Vector3 forwardTranslation = Time.deltaTime * forwardSpeed * Vector3.forward;
-            //Vector3 turbulenceTranslation = GetTurbulenceTranslation();
-            Vector3 translation = forwardTranslation /*+ turbulenceTranslation*/;
-
-            // Movimiento Vertical
-            if ((transform.position.y + deltaVerticalTranslation <= maxY && verticalInput > 0) ||
-                (transform.position.y + deltaVerticalTranslation >= minY && verticalInput < 0))
-            {
-                translation += verticalTranslation;
-            }
-
-            // Movimiento Horizontal
-            if ((transform.position.x <= maxX && horizontalInput > 0) ||
-                (transform.position.x >= minX && horizontalInput < 0))
-            {
-                translation += horizontalTranslation;
-            }
-
-            //Debemos realizar las rotaciones usando espacio global y no local
-            transform.Translate(translation, Space.World);
-        }
-
-        private Vector3 GetTurbulenceTranslation()
-        {
-            float minRange = transform.position.y <= maxY ? +turbulences : 0f;
-            float maxRange = transform.position.y >= minY ? -turbulences : 0f;
-
-            return Random.Range(minRange, maxRange) * Time.deltaTime * Vector3.up;
         }
 
 #if UNITY_EDITOR
@@ -103,19 +99,19 @@ namespace Scripts.Airplanes
             GUIStyle style = new GUIStyle();
             style.fontSize = 24;
             GUI.Label(new Rect(10, 0, 0, 0), "Euler Z: " + transform.rotation.eulerAngles.z, style);
-            GUI.Label(new Rect(10, 20, 0, 0), "Horizontal Input: " + horizontalInput, style);
-            GUI.Label(new Rect(10, 40, 0, 0), "Vertical Input: " + verticalInput, style);
+            GUI.Label(new Rect(10, 20, 0, 0), "Horizontal Input: " + mouseX, style);
+            GUI.Label(new Rect(10, 40, 0, 0), "Vertical Input: " + mouseY, style);
         }
 #endif
 
         internal void Destroy()
         {
-            // Instanciar las partículas y las ubico donde el avión
-            //var ps = Instantiate(particleSystemPrefab, transform.position, transform.rotation);
-            // Hacer que el Airplane sea hijo de las partículas
-            //transform.SetParent(ps.transform);
-            // deshabilitar el Airplane
-            gameObject.SetActive(false);
+        // Instanciar las partículas y las ubico donde el avión
+        //var ps = Instantiate(particleSystemPrefab, transform.position, transform.rotation);
+        // Hacer que el Airplane sea hijo de las partículas
+        //transform.SetParent(ps.transform);
+        // deshabilitar el Airplane
+        gameObject.SetActive(false);
         }
-    }
+}
 }
